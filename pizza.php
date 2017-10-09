121dabf28f47af0f07c0c6abd57c7c525c453ea6
Alejandro Novoa <anovoamontolio@iesesteveterradas.cat>
2017-10-09 18:24:14 +0200

    Intento de funciones

--- pizza.php
+++ pizza.php
@@ -8,6 +8,12 @@
 
   <p><h1>Pizzeria de Buitoni</h1></p>
 
+  <style>
+   
+   
+
+  </style>
+
 <?php
 
   $possibles_ingredients = array(
@@ -27,12 +33,47 @@
 
   for($i=0;$i<$longitud;$i++){
 
-    echo "<p>".$possibles_ingredients[$i]."<input type='checkbox' name='".$i."'value='".$i."'></p>";
+    echo "<p>".$possibles_ingredients[$i]."<input type='checkbox' name='".$i."'value='".$i."'></p>\n";
   }
 
   echo "<button value='submit'>Pedir</button>";
 
   //Funcions
+
+  function processar_dades($ingredients){
+
+  global $possibles_ingredients;
+
+  $preu_total = 5.0;
+
+  //Preparo llista amb tot el que conté la pizza
+  $ingredients_solicitats = "";
+  foreach ($ingredients as $value) {
+    $ingredients_solicitats = $ingredients_solicitats.
+                              "<li>".
+                              $possibles_ingredients[$value].
+                              "</li>";
+    $preu_total += 0.5;
+  }
+
+  //preparo missatge del processament
+  $resultat = "<h1>Has demanat una pizza amb els següents ingredients</h1>
+            <ul>
+               $ingredients_solicitats
+            </ul>
+            <h3>El preu total de la pizza eś $preu_total</h3>
+            ";
+
+  //mostrar resultats
+  echo $resultat;
+}
+
+  function atencio_usuari(){
+
+  echo "<h4>Una bona pizza ha d'incloure massa i orenga</h4>";
+
+}
+
 ?>
 
 </body>
